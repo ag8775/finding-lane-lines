1874ccdabc3c8de68fee6943a6f5f3337cd79ce5
AMER\Manish.Kochhal <manish.kochhal@nio.com>
2017-05-01 20:45:05 -0700

    created jupyter notebook and saved it as html

--- lane-lines-find.py
+++ lane-lines-find.py
@@ -236,8 +236,8 @@ def process_image(image):
     result = pipeline(image)
     return result
 
-white_output = 'test_videos_result/challenge.mp4'
-clip1 = VideoFileClip("test_videos/challenge.mp4")
+white_output = 'test_videos_output/solidWhiteRight.mp4'
+clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
 white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
 white_clip.write_videofile(white_output, audio=False)
 #%time white_clip.write_videofile(white_output, audio=False)
